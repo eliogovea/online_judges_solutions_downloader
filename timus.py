e91e1fd1f2fb0e8f1dcaea53de45c0ff4a7d2a6e
Elio Govea <eliogovea1993@gmail.com>
2019-11-15 16:51:08 -0500

    initial commit: timus

---
+++ timus.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import os
+import getpass
+import requests
+from lxml import html 
+
+def get_judge_number(judge_id):
+    """ I GUESS """
+    judge_number = judge_id
+    while len(judge_number) > 0 and not judge_number.isdigit():
+        judge_number = judge_number[:-1]
+    return judge_number
+
+def get_problem_id_from_url(url):
+    pos = url.find('num=')
+    return url[pos + 4:]
+
+def get_submission_id_from_url(url):
+    patt = 'getsubmit.aspx/'
+    pos = url.find(patt)
+    return url[pos + len(patt):]
+
+def get_submissions_list(session, judge_number, accepted_only = False):
+    # TODO download the next 100 submission again and again ...
+    url = 'http://acm.timus.ru/status.aspx'
+    params = {
+        'author': judge_number,
+        'count': 100 
+    }
+    if accepted_only:
+        params['status'] = 'accepted'
+
+    response = session.get(url, params = params)
+    
+    tree = html.fromstring(response.text)
+    table = tree.xpath('.//table[@class="status"]')
+    rows = table[0].xpath('.//tr')
+
+    submissions = []
+
+    for row in rows:
+        if (not 'even' in row.classes) and (not 'odd' in row.classes):
+            continue
+        submission_url = row.xpath('.//td[@class="id"]/a')[0].attrib['href']
+        problem_url = row.xpath('.//td[@class="problem"]/a')[0].attrib['href']
+        message = 'Accepted'
+        accepted = len(row.xpath('./td[@class="verdict_ac"]')) > 0
+        if not accepted:
+            message = row.xpath('.//td[@class="verdict_rj"]')[0].text
+        submissions.append({
+            'problem_url': problem_url,
+            'submission_url': submission_url,
+            'verdict': message
+        })
+
+    return submissions 
+
+def get_submission_code(session, judge_id, password, url):
+    data = {
+        'Action': 'getsubmit',
+        'JudgeId': judge_id,
+        'Password': password
+    }
+    response = session.post(url, data)
+    return response.text
+
+if __name__ == '__main__':
+    judge_id = input('Judge ID: ')
+    password = getpass.getpass('Password: ')
+    accepted_only = bool(input('Accepted only: '))
+    session = requests.session()
+    print('getting submissions ...')
+    submissions = get_submissions_list(session, get_judge_number(judge_id), accepted_only)
+    print('found {} submissions'.format(len(submissions)))
+    solutions_folder = 'timus_solutions'
+    if not os.path.exists(solutions_folder):
+        os.makedirs(solutions_folder)
+    for submission in submissions:
+        submission_id = get_submission_id_from_url(submission['submission_url'])
+        problem_id = get_problem_id_from_url(submission['problem_url'])
+        print('problem id: {}, submission id: {}'.format(problem_id, submission_id))
+        code = get_submission_code(session, judge_id, password, 'http://acm.timus.ru/' + submission['submission_url'])
+        content = ""
+        content = content + '// {}\n'.format(submission['problem_url'])
+        content = content + '// {}\n'.format(submission['verdict'])
+        content = content + '\n'
+        content = content + code
+        with open('{}/{}-{}'.format(solutions_folder, problem_id, submission_id), 'w', encoding='utf-8') as f:
+            f.write(content)
